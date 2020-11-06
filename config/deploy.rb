8fb911ef0a55d35f2046e313c994777b9df6ef75
marcelo-henrique-sousa <henrique19.marcelo@gmail.com>
2020-11-06 10:23:52 -0300

    asset pipeline erros

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,38 @@ set :migration_role, :app
 set :rails_env, :production
 set :assets_roles, [:web, :app] # Defaults to [:web]
 
+# settings server paths
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :nginx_sites_available_path, "/etc/nginx/sites-available"
+set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+set :rvm_ruby_version, '2.6.6'
+
+# Criamos duas tasks customizadas: uma chamada \puma:create_dirs 
+# e outra chamada puma:nginx_restart e fizemos a chamada delas no final.
+
+# As tasks são responsáveis por criar os diretórios \tmp/pids 
+# e tmp/sockets e por enviar um comando para restart do Nginx.
+
+namespace :puma do
+  desc 'Create Puma dirs'  
+  task :create_dirs do
+    on roles(:app) do      
+      execute "mkdir #{shared_path}/tmp/sockets -p"      
+      execute "mkdir #{shared_path}/tmp/pids -p"    
+    end  
+  end
+  desc "Restart Nginx"
+  task :nginx_restart do
+    on roles(:app) do
+      execute "sudo service nginx restart"
+    end
+  end
+  before :start, :create_dirs
+  after :start, :nginx_restart
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
